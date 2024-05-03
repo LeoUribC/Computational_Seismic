7809a555d9fb9584af5e34ba46984b56ee834183
LeoUribC <leo.uribe1205@gmail.com>
2024-05-03 08:58:56 -0500

    Some spaces added, no major changes on functionality

--- source/DefinedErrors.py
+++ source/DefinedErrors.py
@@ -1,8 +1,8 @@
 
-
 class ContinuationNotDefinedException(Exception):
 
     def __init__(self):
         self.message = "Continuation method must be string either 'v' for continuation\
-             on velocity or 'r' for continuation on receptors"
+             on velocity or 'r' for continuation on receivers"
+        
         super().__init__(self.message)
